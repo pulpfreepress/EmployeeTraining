b8e22bafdd470554d8d653744255c633ab1a6ddf
Rick <rick@pulpfreepress.com>
2020-05-05 11:10:48 -0400

    Refined database scripts for clarity.

---
+++ Database/Scripts/CreateLatestDatabase.bat
@@ -0,0 +1,8 @@
+@echo off
+echo.
+
+rem Call the CreateRelease_3.0_Database.bat file
+echo Calling CreateRelease_3.0_Database.bat file
+call CreateRelease_3.0_Database.bat
+echo Returned from creating database version 3.0
+echo.
